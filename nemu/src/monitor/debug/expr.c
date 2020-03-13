0850050178281fa43bbbe01b5d458f7f7bca84c8
tracer-ics2017 <tracer@njuics.org>
2020-03-13 09:27:58 +0800

    >  compile 1410576 guowei Linux guowei-virtual-machine 4.13.0-36-generic #40~16.04.1-Ubuntu SMP Fri Feb 16 23:26:51 UTC 2018 i686 i686 i686 GNU/Linux  09:27:58 up 21 min,  1 user,  load average: 0.08, 0.36, 0.37 3ad6958fcca4030e6327427d8df3943864512768

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -145,13 +145,13 @@ static bool make_token(char *e)
 
 bool check_parentheses(int p, int q)
 {
-   if(tokens[p].type==6&&tokens[q-1].type==7)
+   if(tokens[p].type==6&&tokens[q].type==7)
    {
       int ori=0;//初始左括号数量，出现右括号会使该值减一
-      if(p<q-2)
+      if(p<q-1)
       {
          int i;
-         for(i=p+1;i<q-1;i++)
+         for(i=p+1;i<q;i++)
          {
             if(tokens[i].type==6)
             ori++;
