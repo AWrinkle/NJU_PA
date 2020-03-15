ea3d30139b7e131e120558c24037cb7e50d27392
tracer-ics2017 <tracer@njuics.org>
2020-03-15 16:00:49 +0800

    >  compile 1410576 guowei Linux guowei-virtual-machine 4.13.0-36-generic #40~16.04.1-Ubuntu SMP Fri Feb 16 23:26:51 UTC 2018 i686 i686 i686 GNU/Linux  16:00:49 up  1:33,  1 user,  load average: 0.12, 0.18, 0.13 b2049ea8561772001da9236b7c75039ce92d8492

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -404,6 +404,7 @@ uint32_t expr(char *e, bool *success) {
   /* TODO: Insert codes to evaluate the expression. */
   //TODO();
   int terr=eval(0,nr_token-1);
+  *success=true;
   if(Bad==Bad_Expression)
   {
     *success = false;
