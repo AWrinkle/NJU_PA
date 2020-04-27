5f58ddf3d8650284d2661fbad932a4f9a0dc30dc
tracer-ics2017 <tracer@njuics.org>
2020-04-27 11:46:26 +0800

    >  compile 1410576 guowei Linux guowei-virtual-machine 4.15.0-96-generic #97~16.04.1-Ubuntu SMP Wed Apr 1 03:01:48 UTC 2020 i686 i686 i686 GNU/Linux  11:46:26 up  1:59,  1 user,  load average: 0.05, 0.10, 0.08 8ff3bd361e02ccbcb00368dd84274846f9d7e140

--- nemu/src/memory/memory.c
+++ nemu/src/memory/memory.c
@@ -13,6 +13,8 @@ uint8_t pmem[PMEM_SIZE];
 
 uint32_t paddr_read(paddr_t addr, int len) {
   int r=is_mmio(addr);
+  printf("%X/n",addr);
+  printf("%d",r);
   if(r==-1)
           return pmem_rw(addr, uint32_t) & (~0u >> ((4 - len) << 3));
   else
