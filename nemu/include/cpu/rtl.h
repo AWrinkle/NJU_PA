03b0a1ab19821880a5a0adaf7e686691cc295ec0
tracer-ics2017 <tracer@njuics.org>
2020-04-27 18:41:41 +0800

    >  compile 1410576 guowei Linux guowei-virtual-machine 4.15.0-96-generic #97~16.04.1-Ubuntu SMP Wed Apr 1 03:01:48 UTC 2020 i686 i686 i686 GNU/Linux  18:41:41 up  8:44,  1 user,  load average: 0.22, 0.24, 0.15 218a6d34e2bc93874b0b897b6b4b79b8b025637

--- nemu/include/cpu/rtl.h
+++ nemu/include/cpu/rtl.h
@@ -141,6 +141,7 @@ static inline void rtl_not(rtlreg_t* dest) {
 static inline void rtl_sext(rtlreg_t* dest, const rtlreg_t* src1, int width) {
   // dest <- signext(src1[(width * 8 - 1) .. 0])
   // TODO()，符号扩展;
+  /*
   if(width==4)
   {
         rtl_mv(dest,src1);
@@ -151,6 +152,10 @@ static inline void rtl_sext(rtlreg_t* dest, const rtlreg_t* src1, int width) {
         rtl_shli(dest,src1,(4-width)*8);
         rtl_sari(dest,dest,(4-width)*8);
   }
+  */
+  rtl_li(&t1,32-width*8);
+  rtl_shl(dest,src1,&t1);
+  rtl_sar(dest,dest,&t1);
 }
 
 static inline void rtl_push(const rtlreg_t* src1) {
