383b40a08d5717ef27d40055b1aeecce72c927e7
tracer-ics2017 <tracer@njuics.org>
2020-05-18 17:20:10 +0800

    >  compile 1410576 guowei Linux guowei-virtual-machine 4.15.0-99-generic #100~16.04.1-Ubuntu SMP Wed Apr 22 23:55:42 UTC 2020 i686 i686 i686 GNU/Linux  17:20:10 up  1:14,  1 user,  load average: 0.12, 0.19, 0.09 df5cbfa748be07c28c57eeeacf7290b76635591e

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -1,8 +1,14 @@
 #include "common.h"
 
+extern void ramdisk_read(void *buf, off_t offset, size_t len);
+extern size_t get_ramdisk_size();
 #define DEFAULT_ENTRY ((void *)0x4000000)
 
 uintptr_t loader(_Protect *as, const char *filename) {
-  TODO();
+  //TODO();
+  size_t size = get_ramdisk_size();
+  void * buff = NULL;
+  ramdisk_read(buff,0,size); 
+  memcpy(DEFAULT_ENTRY,buff,size);
   return (uintptr_t)DEFAULT_ENTRY;
 }
