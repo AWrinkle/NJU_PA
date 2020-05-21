dcf8972a285cc1c1d23f9c8aeaada4bf8a73390c
tracer-ics2017 <tracer@njuics.org>
2020-05-21 11:46:59 +0800

    >  run 1410576 guowei Linux guowei-virtual-machine 4.15.0-101-generic #102~16.04.1-Ubuntu SMP Mon May 11 11:44:23 UTC 2020 i686 i686 i686 GNU/Linux  11:46:59 up  1:28,  1 user,  load average: 0.10, 0.09, 0.06 3c3272559e224969d3c9746aa6604857160dc297

--- nexus-am/am/arch/x86-nemu/src/asye.c
+++ nexus-am/am/arch/x86-nemu/src/asye.c
@@ -28,9 +28,9 @@ static GateDesc idt[NR_IRQ];
 
 void _asye_init(_RegSet*(*h)(_Event, _RegSet*)) {
   // initialize IDT
- // for (unsigned int i = 0; i < NR_IRQ; i ++) {
- //   idt[i] = GATE(STS_TG32, KSEL(SEG_KCODE), vecnull, DPL_KERN);
- // }
+  for (unsigned int i = 0; i < NR_IRQ; i ++) {
+    idt[i] = GATE(STS_TG32, KSEL(SEG_KCODE), vecnull, DPL_KERN);
+  }
 
   // -------------------- system call --------------------------
   idt[0x80] = GATE(STS_TG32, KSEL(SEG_KCODE), vecsys, DPL_USER);
