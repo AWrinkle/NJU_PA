59c23cc46fc7494cdcea811ef972ffd40318fc01
tracer-ics2017 <tracer@njuics.org>
2020-05-24 16:34:23 +0800

    >  run 1410576 guowei Linux guowei-virtual-machine 4.15.0-101-generic #102~16.04.1-Ubuntu SMP Mon May 11 11:44:23 UTC 2020 i686 i686 i686 GNU/Linux  16:34:23 up  1:03,  1 user,  load average: 0.33, 0.15, 0.07 2257f60e24c86d45ceaf98e1df29446a21fe4c10

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -20,6 +20,7 @@ int sys_write(int fd,void* buf,size_t len)
   if(fd==1||fd==2)
   {
      char c;
+     Log("buffer:%s",(char*)buf);
      for(int i=0;i<len;i++)
      {
         memcpy(&c,buf+i,1);
@@ -32,6 +33,10 @@ int sys_write(int fd,void* buf,size_t len)
   return -1;
 }
 
+int sys_brk(int addr)
+{
+  return 0;
+}
 
 _RegSet* do_syscall(_RegSet *r) {
   uintptr_t a[4];
@@ -47,6 +52,9 @@ _RegSet* do_syscall(_RegSet *r) {
       return sys_exit(r);
     case SYS_write:
       SYSCALL_ARG1(r)=sys_write(a[1],(void*)a[2],a[3]);
+      break;
+    case SYS_brk:
+      SYSCALL_ARG1(r)=sys_brk(a[1]);
       break;
     default: panic("Unhandled syscall ID = %d", a[0]);
   }
