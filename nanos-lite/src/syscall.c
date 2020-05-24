7442355d6744d5bed3b545f81ca2d66d81a22862
tracer-ics2017 <tracer@njuics.org>
2020-05-24 12:25:18 +0800

    >  run 1410576 guowei Linux guowei-virtual-machine 4.15.0-101-generic #102~16.04.1-Ubuntu SMP Mon May 11 11:44:23 UTC 2020 i686 i686 i686 GNU/Linux  12:25:18 up  1:04,  1 user,  load average: 0.26, 0.10, 0.06 a83d209a50abefabd8686f2fef679214589c16ff

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -1,6 +1,8 @@
 #include "common.h"
 #include "syscall.h"
 
+
+
 static inline _RegSet* sys_none(_RegSet *r){
   SYSCALL_ARG1(r) = 1; //约定系统调用返回值存于此，即eax
   return NULL;
@@ -10,6 +12,24 @@ static inline _RegSet* sys_exit(_RegSet *r){
   //接受一个退出状态的参数，顺序ebx ecx edx
   _halt(SYSCALL_ARG2(r)); 
   return NULL;
+}
+
+
+int sys_write(int fd,void* buf,size_t len)
+{
+  if(fd==1||fd==2)
+  {
+     char c;
+     for(int i=0;i<len;i++)
+     {
+        memcpy(&c,buf+i,1);
+        _putc(c);
+     }
+     return len;
+  }
+  else
+     panic("Unhandled fd =%d in sys_write",fd);
+  return -1;
 }
 
 
@@ -25,6 +45,9 @@ _RegSet* do_syscall(_RegSet *r) {
       return sys_none(r);
     case SYS_exit:
       return sys_exit(r);
+    case SYS_write:
+      SYSCALL_ARG1(r)=sys_write(a[1],(void*)a[2],a[3]);
+      break;
     default: panic("Unhandled syscall ID = %d", a[0]);
   }
 
