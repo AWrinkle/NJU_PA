f1e0050b3a29edf4b914966790361a0ac87ea312
tracer-ics2017 <tracer@njuics.org>
2020-06-02 20:54:57 +0800

    >  compile 1410576 guowei Linux guowei-virtual-machine 4.15.0-101-generic #102~16.04.1-Ubuntu SMP Mon May 11 11:44:23 UTC 2020 i686 i686 i686 GNU/Linux  20:54:57 up  5:45,  1 user,  load average: 0.26, 0.16, 0.10 c620238c96d8d1553045992deaaa0d9aad1e23f

--- nemu/src/cpu/exec/system.c
+++ nemu/src/cpu/exec/system.c
@@ -29,7 +29,7 @@ make_EHelper(mov_r2cr) {
     cpu.cr0.val=reg_l(id_src->reg);
   else if(id_dest->reg==3)
     cpu.cr3.val=reg_l(id_src->reg);
-  
+  Log("cr3=0x%x,cr0=0x%x,id_src=0x%x",cpu.cr3.val,cpu.cr0.val,reg_l(id_src->reg));
   print_asm("movl %%%s,%%cr%d", reg_name(id_src->reg, 4), id_dest->reg);
 }
 
