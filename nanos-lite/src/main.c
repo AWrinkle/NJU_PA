f5a2237856aba7b06393febaefa988792d58e336
tracer-ics2017 <tracer@njuics.org>
2020-06-06 11:11:16 +0800

    >  run 1410576 guowei Linux guowei-virtual-machine 4.15.0-101-generic #102~16.04.1-Ubuntu SMP Mon May 11 11:44:23 UTC 2020 i686 i686 i686 GNU/Linux  11:11:16 up 2 min,  1 user,  load average: 0.52, 0.39, 0.16 563de28547feadf57fd56eb51abbf5a7f548ad47

--- nanos-lite/src/main.c
+++ nanos-lite/src/main.c
@@ -33,7 +33,7 @@ int main() {
 
   //uint32_t entry = loader(NULL, "/bin/pal");
   
-  load_prog("/bin/hello");
+  load_prog("/bin/bmp");
   //((void (*)(void))entry)();
 
   panic("Should not reach here");
