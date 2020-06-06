77e596717b826e4e7ceef179243fd17a2cf3c6b8
tracer-ics2017 <tracer@njuics.org>
2020-06-06 10:12:30 +0800

    >  run 1410576 guowei Linux guowei-virtual-machine 4.15.0-101-generic #102~16.04.1-Ubuntu SMP Mon May 11 11:44:23 UTC 2020 i686 i686 i686 GNU/Linux  10:12:30 up  1:09,  1 user,  load average: 0.21, 0.19, 0.09 f3aa9a7bcb0b9e1996755ff7e0bbc96e6c0cde34

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -9,31 +9,7 @@ extern void* new_page(void);
 //extern uint8_t ramdisk_end;
 #define DEFAULT_ENTRY ((void *)0x8048000)
 
-/*
-uintptr_t loader(_Protect *as, const char *filename) {
-  //TODO();
-  //size_t size = get_ramdisk_size();
-  //ramdisk_read(DEFAULT_ENTRY,0,size);
-  int fd=fs_open(filename,0,0);
-  //Log("filename=%s,fd=%d",filename,fd);
-  //fs_read(fd,DEFAULT_ENTRY,fs_filesz(fd));
-  //fs_close(fd);
-   
-  int len=fs_filesz(fd);
-  void* pa;
-  void* va;
-  Log("filename=%s,fd=%d,size:%d\n",filename,fd,len);
-  void* end=DEFAULT_ENTRY+len;
-  for(va=DEFAULT_ENTRY;va<=end-PGSIZE;va+=PGSIZE)
-  {
-    pa=new_page();
-    _map(as,va,pa);
-    fs_read(fd,pa,PGSIZE);
-  }
 
-  return (uintptr_t)DEFAULT_ENTRY;
-}
-*/
 
 uintptr_t loader(_Protect *as, const char *filename) {
   int fd = fs_open(filename, 0, 0);
