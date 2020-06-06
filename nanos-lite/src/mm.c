303b24893f747a77b8a01b588c15d7fa3ecdcec3
tracer-ics2017 <tracer@njuics.org>
2020-06-06 10:26:51 +0800

    >  run 1410576 guowei Linux guowei-virtual-machine 4.15.0-101-generic #102~16.04.1-Ubuntu SMP Mon May 11 11:44:23 UTC 2020 i686 i686 i686 GNU/Linux  10:26:51 up  1:24,  1 user,  load average: 0.19, 0.15, 0.13 7882fe7128ba9fc68b6a530c184ad4cca4948888

--- nanos-lite/src/mm.c
+++ nanos-lite/src/mm.c
@@ -15,19 +15,27 @@ void free_page(void *p) {
 }
 
 /* The brk() system call handler. */
-#define K4(va) (((uint32_t)(va)+0xfff) & ~0xfff)
-int mm_brk(uint32_t new_brk) {
-	if (current->cur_brk == 0) {
+
+int mm_brk(uint32_t new_brk) 
+{
+	if (current->cur_brk == 0)
+        {
 		current->cur_brk = current->max_brk = new_brk;
-	} else {
-		if (new_brk > current->max_brk) {
+	} 
+        else 
+        {
+		if (new_brk > current->max_brk) 
+                {
 	        	// TODO: map memory region [current->max_brk, new_brk)
 	        	// into address space current->as
-			uintptr_t va = K4(current->max_brk);//4k对齐
-	        	while(va < new_brk) {
-                		_map(&current->as, (void *)va, (void *)new_page());
-				va += PGSIZE;
-			}
+			uintptr_t pa,va;
+                        for(va=(current->max_brk+0xfff) & ~0xfff;va<new_brk;va+=PGSIZE)
+                        {
+                           pa=(uintptr_t)new_page();
+                           _map(&current->as,(void *)va,(void *)pa);
+                        }
+                        
+                           
 			current->max_brk = new_brk; // =va ?
 		}
 	current->cur_brk = new_brk;
