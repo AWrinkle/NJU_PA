c1429ade4dfcb57673a38228d232512960421a11
tracer-ics2017 <tracer@njuics.org>
2020-06-09 16:14:02 +0800

    >  run 1410576 guowei Linux guowei-virtual-machine 4.15.0-101-generic #102~16.04.1-Ubuntu SMP Mon May 11 11:44:23 UTC 2020 i686 i686 i686 GNU/Linux  16:14:02 up  1:45,  1 user,  load average: 0.75, 0.48, 0.32 14b1c0c4fb1f0fc266237acb7693d94dc3570cde

--- nanos-lite/src/irq.c
+++ nanos-lite/src/irq.c
@@ -13,7 +13,7 @@ static _RegSet* do_event(_Event e, _RegSet* r) {
     case _EVENT_TRAP: return schedule(r);
       //printf("receive an event trap!!!");break;
     case _EVENT_IRQ_TIME:
-      Log("event:IRQ_TIME");
+      //Log("event:IRQ_TIME");
       return schedule(r);
     default: panic("Unhandled event ID = %d", e.event);
   }
