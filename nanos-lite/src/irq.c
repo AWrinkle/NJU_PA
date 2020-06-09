6b37d29862a3a6b260f847b91e4aad1464b14cbc
tracer-ics2017 <tracer@njuics.org>
2020-06-09 16:25:57 +0800

    >  run 1410576 guowei Linux guowei-virtual-machine 4.15.0-101-generic #102~16.04.1-Ubuntu SMP Mon May 11 11:44:23 UTC 2020 i686 i686 i686 GNU/Linux  16:25:57 up  1:57,  1 user,  load average: 0.18, 0.31, 0.38 2c904d692a7bc3141be7d48aff705d65a3c18a1

--- nanos-lite/src/irq.c
+++ nanos-lite/src/irq.c
@@ -9,7 +9,7 @@ static _RegSet* do_event(_Event e, _RegSet* r) {
   switch (e.event) {
     case _EVENT_SYSCALL:
        do_syscall(r);
-       return schedule(r);
+       //return schedule(r);
     case _EVENT_TRAP: return schedule(r);
       //printf("receive an event trap!!!");break;
     case _EVENT_IRQ_TIME:
